a5fa516b63782695a35f29d5f632af0832386835
Sandeep <96532946+Sandyfre@users.noreply.github.com>
2025-05-24 06:55:27 +0000

    datatype updated

--- 01_basics/02dataTypes.js
+++ 01_basics/02dataTypes.js
@@ -1,3 +1,9 @@
 "use Strict" // use all the js code in newer version
 
 // alert("hello") // we are using node JS not brouser
+
+//boolean => true/ false
+//string => ""
+//null => null is a stand alone value
+// undefined =>
+// Symbol => is for unqueness    
