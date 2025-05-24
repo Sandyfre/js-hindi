c17e2b7ae1f83bba857d7f6e8caf199ae2ff72f5
Sandeep <96532946+Sandyfre@users.noreply.github.com>
2025-05-24 09:20:29 +0000

    conversion of data Type

---
+++ 01_basics/03_conversionsAndOperations.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "23" => 23
+//"23abc" => NaN
+// true = 1
+//false = 0
+
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+
+let someNumber  = 37
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
