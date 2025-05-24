d82cef1a3b27cfe88fda0ad1d1c1d14d03057abc
Sandeep <96532946+Sandyfre@users.noreply.github.com>
2025-05-24 17:10:01 +0000

    conversion

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -14,10 +14,34 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = 0
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+//console.log(booleanisLoggedIn);
 
 
 let someNumber  = 37
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************ Operations ******************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1 = "Welcome"
+let str2 = " Sandeep"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log(1+ "2"+2);
+console.log(1+ 2+"2");
