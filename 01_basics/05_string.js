dd8abf146f3e6ccf00af7d7e3dbc5b3951cf923b
Sandeep <96532946+Sandyfre@users.noreply.github.com>
2025-05-25 23:28:02 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const Name = 'Sandeep '
+const repoCount = 50
+
+// console.log(Name + repoCount + " Value");
+
+
+//  console.log(`My name is ${Name.toLocaleUpperCase()} and my repo count is ${repoCount}`);
+
+
+const myGameName = new String ("Sandeeppubg")
+
+// console.log(myGameName[0]);
+// console.log(myGameName.__proto__);
+
+
+// console.log(myGameName.length);
+// console.log(myGameName.toLocaleUpperCase());
+// console.log(myGameName.charAt(5));
+
+
+const name = "     Sandeep      "
+
+console.log(name);
+console.log(name.trim());
+
+
+
+
