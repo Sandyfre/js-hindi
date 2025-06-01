0b36756e3516f240bcba45c0ed81e4e4c4e64c3b
Sandeep <96532946+Sandyfre@users.noreply.github.com>
2025-06-01 18:04:56 +0000

    number and date

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,4 @@
+const score = 400
+
+const balance = new Number(900000000000000)
+console.log(balance.toLocaleString("en-In"));
---
+++ 01_basics/07_datesIn.js
@@ -0,0 +1,7 @@
+// Date
+ let myDate = new Date()
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toJSON());
+ console.log(myDate.toLocaleString());
+ 
